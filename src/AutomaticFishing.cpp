cc0ad907185af1d3ea2f8353f97d98c5ca5fd283
子沐呀 <163636894+zimuya4153@users.noreply.github.com>
2024-07-30 03:27:04 +0800

    去除了不必要的代码

--- src/AutomaticFishing.cpp
+++ src/AutomaticFishing.cpp
@@ -5,74 +5,32 @@
 #include <ll/api/schedule/Scheduler.h>
 #include <ll/api/schedule/Task.h>
 #include <ll/api/service/Bedrock.h>
-#include <mc/nbt/CompoundTag.h>
 #include <mc/world/actor/FishingHook.h>
 #include <mc/world/actor/player/Player.h>
 #include <mc/world/item/Item.h>
-#include <mc/world/item/enchanting/Enchant.h>
-#include <mc/world/item/enchanting/EnchantUtils.h>
 #include <mc/world/level/Level.h>
 #include <memory>
 
-
 ll::schedule::GameTickScheduler tickScheduler;
-std::map<FishingHook*, int>     fishingHook;
 
-int randomInt() {
-    static std::random_device                 rd;
-    static std::default_random_engine         e(rd());
-    static std::uniform_int_distribution<int> dist(0, 99);
-    return dist(e);
-}
-
-void ReduceItemDurability(ItemStack& item, Player* player) {
-    int durabilityLevel = EnchantUtils::getEnchantLevel(::Enchant::Type::MiningDurability, item);
-    if (auto tag = item.save()->getCompound("tag")) {
-        if (tag->getByte("Unbreakable")) {
-            return;
-        }
-    }
-    if (durabilityLevel == 0 || (durabilityLevel > 0 && randomInt() < (100 / durabilityLevel + 1))) {
-        if (item.getMaxDamage() == item.getDamageValue() - 1) {
-            item.setNull("");
-            player->setSelectedItem(item);
-            player->playSound(Puv::Legacy::LevelSoundEvent::ItemUseOn, player->getPosition(), 1);
-            return;
-        }
-        item.setDamageValue(item.getDamageValue() + 1);
-        player->setSelectedItem(item);
-    }
-}
-
-LL_TYPE_INSTANCE_HOOK(PlayerPullFishingHook, HookPriority::Normal, FishingHook, &FishingHook::_updateServer, void) {
-    origin();
-    auto player = getPlayerOwner();
-    if (_serverHooked() && !fishingHook.count(this)) {
-        fishingHook[this] = 0;
-        return;
-    }
-    if (_serverHooked() && fishingHook[this] == 0) {
-        fishingHook[this] = 1;
-        return;
-    }
-    if (!_serverHooked() && fishingHook[this] == 1) {
-        auto item = player->getSelectedItem();
-        item.getItem()->use(item, *player);
-        fishingHook.erase(this);
-        if (player->getPlayerGameType() != GameType::Creative) {
-            ReduceItemDurability(item, player);
-        }
+LL_TYPE_INSTANCE_HOOK(PlayerPullFishingHook, HookPriority::Normal, FishingHook, &FishingHook::_serverHooked, bool) {
+    auto rtn = origin();
+    if (rtn && *((int*)this + 284) == 0) {
+        auto  player = getPlayerOwner();
+        auto& item   = player->getSelectedItem();
+        item.getItem()->use(const_cast<ItemStack&>(item), *player);
         player->refreshInventory();
         auto& uid = player->getUuid();
         tickScheduler.add<ll::schedule::DelayTask>(ll::chrono_literals::operator""_tick(10), [uid]() {
             auto player = ll::service::getLevel()->getPlayer(uid);
-            if (!player->isPlayer()) return;
-            auto item = player->getSelectedItem();
+            if (!player) return;
+            auto& item = player->getSelectedItem();
             if (!item.isNull() && item.getTypeName() == "minecraft:fishing_rod") {
-                item.getItem()->use(item, *player);
+                item.getItem()->use(const_cast<ItemStack&>(item), *player);
             }
         });
     }
+    return rtn;
 }
 
 namespace AutomaticFishing {
